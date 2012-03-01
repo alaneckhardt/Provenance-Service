4b53a6eb107c62ea76aabfcd50b21035e05ef6c0
Alan Eckhardt <a.e@centrum.cz>
2012-03-01 14:54:20 +0000

    Fixed the directions of relationship - from=cause, to=effect
    Change-Id: I5b147a462d61abb36e509eaf920671164f2f2dc2

--- src/main/provenanceService/ProvenanceService.java
+++ src/main/provenanceService/ProvenanceService.java
@@ -519,8 +519,8 @@ public class ProvenanceService  extends javax.servlet.http.HttpServlet implement
 		Resource e = model.getResource(effect);
 		Resource r = model.getResource(type);
 		model.add(relationship, RDF.type, r);
-		model.add(relationship, RDFProvider.getProp("to"), c);
-		model.add(relationship, RDFProvider.getProp("from"), e);
+		model.add(relationship, RDFProvider.getProp("from"), c);
+		model.add(relationship, RDFProvider.getProp("to"), e);
 		return relationship.getURI();
 	}
 	
